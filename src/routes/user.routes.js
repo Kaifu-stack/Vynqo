8c77e74c0573877bf3ad20dc6e6298769bf58b91
Kaifu-stack <kaif22101@gmail.com>
2026-01-26 18:49:34 +0530

    Add New routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,21 @@
 import { Router } from "express";
-import { loginuser, logoutuser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    loginuser,
+    logoutuser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateuserAvatar,
+    updateuserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+}
+    from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 
 const router = Router();
 
@@ -25,4 +39,18 @@ router.route("/login").post(loginuser)
 router.route("/logout").post(verifyJWT, logoutuser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-passsword").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateuserAvatar)
+
+router.route("/cover-Image").patch(verifyJWT, upload.single("/coverImage"), updateuserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watchHistory").get(verifyJWT, getWatchHistory)
 export default router;
