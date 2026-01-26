31ecf3b029479347a6a9963b4228c597f79baf56
Kaifu-stack <kaif22101@gmail.com>
2026-01-26 18:40:07 +0530

    Add getWatchHistory EndPoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -380,6 +380,55 @@ const getUserChannelProfile = asynchandler(async (req, res) => {
     return res.status(200)
         .json(new apiResponse(200, channel[0], "user channel fetched successfully"))
 })
+
+const getWatchHistory = asynchandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.Object_ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos", // mongo db converts to lowercase pluralform
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                },
+                                {
+                                    $addFields: {
+                                        owner: {
+                                            $first: "$owner"
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res
+        .status(200)
+        .json(
+            new apiResponse(200, user[0].watchHistory, "watch History Fetched Successfully")
+        )
+})
 export {
     registerUser,
     loginuser,
@@ -390,5 +439,6 @@ export {
     updateAccountDetails,
     updateuserAvatar,
     updateuserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 };
