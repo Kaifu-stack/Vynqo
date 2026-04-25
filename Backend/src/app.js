602b24f830dad7ed862d0fcdf683ea1a60d3dcfb
Kaifu-stack <kaif22101@gmail.com>
2026-04-25 22:33:04 +0530

    modified

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,11 +3,22 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://vynqo.vercel.app"
+];
 app.use(cors({
-    origin: true,
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true
 }));
-
 app.use(express.json({
     limit: "16kb"
 }));
